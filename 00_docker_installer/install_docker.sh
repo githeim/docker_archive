f131acf6992d049a5a4e1aea3921195e97b78759
nowhere <nowhere@nowhere.com>
2023-08-11 21:23:25 +0900

    update docker & nvidia docker installation scripts

--- 00_docker_installer/install_docker.sh
+++ 00_docker_installer/install_docker.sh
@@ -1,35 +1,11 @@
-#!/bin/sh
-trap error_callback ERR
-                       
-error_callback() {     
-  echo 'Error Occurs'  
-  exit 1
-}                      
+#!/bin/bash
+err_report() {
+  echo "Error on line $1"
+  exit 1 
+}
+trap 'err_report $LINENO' ERR
 
-sudo apt-get remove docker docker-engine -y
-sudo apt-get update -y
 
-sudo apt-get install \
-  linux-image-extra-$(uname -r) \
-      linux-image-extra-virtual -y
+curl -fsSL https://get.docker.com -o get-docker.sh
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) \
-         stable"
-sudo apt-get update -y
-sudo apt-get install docker-ce -y
-
-# docker machine 설치
-curl -L https://github.com/docker/machine/releases/download/v0.12.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
-
-# install nvidia docker toolkit
-./install_nvidia_docker_toolkit.sh
+sudo sh get-docker.sh
--- 00_docker_installer/install_nvidia_docker_toolkit.sh
+++ 00_docker_installer/install_nvidia_docker_toolkit.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+err_report() {
+  echo "Error on line $1"
+  exit 1 
+}
+trap 'err_report $LINENO' ERR
+
 # install nvidia docker toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+
 sudo apt-get update                                                              
-sudo apt-get install -y nvidia-docker2                                           
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
 
