6b618fbbfe02c2b317cfe1c52f95ac9b507066b2
githeim <githeim@nowhere.com>
2025-05-01 13:16:27 +0900

    update install nvidia docker script

--- 00_docker_installer/install_nvidia_docker_toolkit.sh
+++ 00_docker_installer/install_nvidia_docker_toolkit.sh
@@ -5,13 +5,12 @@ err_report() {
 }
 trap 'err_report $LINENO' ERR
 
-# install nvidia docker toolkit
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-
+# install nvidia container toolkit
+echo installation script version 2025.03.13
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 sudo apt-get update                                                              
 sudo apt-get install -y nvidia-container-toolkit
